09d226bad3efdf7b4a7a967c59016933e6eee837
Ivan Semenov <i.semenov8@yandex.ru>
2021-10-04 15:42:02 +0300

    add swiper

--- build/js/main.js
+++ build/js/main.js
@@ -103,29 +103,33 @@ window.addEventListener('keydown', function (evt) {
   }
 });
 
+
+// -------------------Swiper-----------------------
+
+const swiper = new Swiper('.swiper', {
+
+  breakpoints: {
+        320: {
+                slidesPerView: 2,
+                // spaceBetween: 30
+              },
+        1024: {
+          slidesPerView: 4,
+          // spaceBetween: 30
+        }
+      },
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  navigation: {
+    nextEl: '.new__button--left',
+    prevEl: '.new__button--right',
+  },
+});
+
+
 })();
 
 
-// const swiper = new Swiper('.swiper', {
-//   // Default parameters
-//   slidesPerView: 1,
-//   spaceBetween: 10,
-//   // Responsive breakpoints
-//   breakpoints: {
-//     // when window width is >= 320px
-//     320: {
-//       slidesPerView: 2,
-//       spaceBetween: 20
-//     },
-//     // when window width is >= 480px
-//     480: {
-//       slidesPerView: 3,
-//       spaceBetween: 30
-//     },
-//     // when window width is >= 640px
-//     640: {
-//       slidesPerView: 4,
-//       spaceBetween: 40
-//     }
-//   }
-// })
