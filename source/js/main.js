4396a76d223c74d104d9bb44328e53d25976e36b
Ivan Semenov <i.semenov8@yandex.ru>
2021-10-15 11:50:08 +0300

    4 last

--- source/js/main.js
+++ source/js/main.js
@@ -15,13 +15,11 @@ navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--opened');
 
   if (navMain.classList.contains('main-nav--opened')) {
-    // pageBody.style.overflow = 'hidden';
     promo.style.display = 'none';
     pageWrapper.style.display = 'none';
   } else {
     promo.style.display = 'flex';
     pageWrapper.style.display = 'block';
-    // pageBody.style.overflow = 'auto';
   }
 });
 
@@ -249,4 +247,38 @@ if (itemProduct) {
     itemPrice.classList.toggle('catalog__form-item--nojs');
   });
 }
+
+
+// --------------------trap focus inside modal login-----------------------------
+
+const  focusableElements ='button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+const modal = document.querySelector('#modal');
+
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0];
+const focusableContent = modal.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+document.addEventListener('keydown', function(e) {
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (e.shiftKey) {
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus();
+      e.preventDefault();
+    }
+  } else {
+    if (document.activeElement === lastFocusableElement) {
+      firstFocusableElement.focus();
+      e.preventDefault();
+    }
+  }
+});
+
+firstFocusableElement.focus();
+
+
 })();
